746ce5ee27139fd12a3e7479c30ad20be65fa8f2
PonPrasanth10 <ponprasanthr@gmail.com>
2026-03-08 20:55:34 +0530

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ connectDB();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'https://vikas-frontend-eta.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
