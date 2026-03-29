adbb24eed5ad49fee0e9db948104bd48d8f79447
PonPrasanth10 <ponprasanthr@gmail.com>
2026-03-29 22:06:37 +0530

    origin fix

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(helmet());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://vikas-frontend-eta.vercel.app'
+    'https://vikas-frontend-eta.vercel.app',
+    'www.vikastrendz.com'
   ],
   credentials: true
 }));
